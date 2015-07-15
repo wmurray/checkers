02352e0df7828fbc0c3477078953b18937e3c8ca
Murray <will.murray@BOSGEN-NMX0689.local>
2015-07-14 23:34:29 -0400

    Building board sans canvas, using dom nodes.

--- scripts/board.js
+++ scripts/board.js
@@ -5,111 +5,23 @@ $(document).ready(function(){
   /////////////////
 
   function Game(){
-    var canvas;
-    var ctx;
-    var WIDTH;
-    var HEIGHT;
-    var INTERVAL = 20; // check for redraw every (this) milliseconds
 
-    var isDrag = false;
+    var boardbox = document.getElementById('boardBox');
 
-    var mouseX, mouseY; // Mouse coords
-
-    var canvasValid = false; // Determines if canvas needs redraw
-
-    // Current selection (if any), its border color and width.
-    var selected;
-    var selectedColor = '#000000';
-    var selectedBorderWidth = 2;
-
-    // Shadow canvas is used for selection.
-    var shadowCanvas;
-    var shadowCtx;
-
-    // Mouse offset for dragging (you don't have to click center of piece to
-    // drag)
-    var offsetX, offsetY;
-
-    // Padding and border style widths for mouse offset.
-    var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
-
-    var boardBox = document.querySelector('.board-box');
-    var pieces = [];
-    this.startButton = document.getElementsByClassName('button');
-
-    function makeCanvas(){
-      var oldGame = document.getElementById('board');
-      var newCanvas = document.createElement('canvas');
-
-      if (oldGame !== null){
-        boardBox.removeChild(oldGame);
-      };
-
-      newCanvas.id = 'board';
-      newCanvas.width = 400;
-      newCanvas.height = 400;
-
-      boardBox.appendChild(newCanvas);
-    }
-
-    function initGame(){
-      canvas = document.getElementById('board');
-      HEIGHT = canvas.height;
-      WIDTH = canvas.width;
-      ctx = canvas.getContext('2d');
-      shadowCanvas = document.createElement('canvas');
-      shadowCanvas.height = HEIGHT;
-      shadowCanvas.width = WIDTH;
-      shadowCanvasCtx = shadowCanvas.getContext('2d');
-
-      // Fix for double-click selecting text in canvas.
-      canvas.onselectstart = function(){ return false; }
-
-      // Fix for mouse coordinates w/ border/padding
-      // see also getMouse
-      if (document.defaultView && document.defaultView.getComputedStyle){
-        stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['padding-left'], 10) || 0;
-        stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10) || 0;
-        styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10) || 0;
-        styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10) || 0;
-      }
-
-      // make draw() fire every INTERVAL milliseconds
-      setInterval(draw, INTERVAL);
-
-      canvas.onMouseDown = mouseDown;
-      canvas.onMouseUp = mouseUp;
-
-      new Piece(50, 100, 25, 25, 'yellow');
-    }
-
-    function draw(){
-      if (canvasValid == false){
-        clear(ctx);
-      }
-    }
+    var initGame = function(){
+      new Board(boardBox);
+    };
 
     $('.button').on('click', function(){
-      makeCanvas();
       initGame();
-    })
+    });
   }
 
   //////////////////
   // Piece object //
   //////////////////
 
-  function Piece(x, y, width, height, fill){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.fill = fill;
-
-    (function trackPiece(){
-      pieces.push(this);
-      invalidate();
-    })();
+  function Piece(){
   }
 
   //////////////////
@@ -122,33 +34,52 @@ $(document).ready(function(){
     var self = this;
 
     (function startNewGame(){
+      console.log('board invoked');
       self.makeBoard();
     })();
   }
 
   Board.prototype.makeBoard = function(){
-    for(var r = 0; r < 8; r++){
-      for(var c = 0; c < 8; c++){
-        var x = c * 50;
-        var y = r * 50;
 
-        if (r % 2 === 0){
-          if (c % 2 === 0){
-            ctx.fillStyle = 'green';
-          } else {
-            ctx.fillStyle = 'white';
-          }
-        } else {
-          if (c % 2 === 0){
-            ctx.fillStyle = 'white';
-          } else {
-            ctx.fillStyle = 'green';
+    for(var i = 0; i < 64; i++){
+      var square = document.createElement('div');
+      square.className = 'space';
+      boardBox.appendChild(square);
+    }
+
+    var spaces = document.getElementsByClassName('space');
+    var spacesLength = spaces.length;
+    var spacesCoords = [];
+
+    var paintBoard = function(){
+      var miniArray = [];
+
+      for (var i = 0; i <= spaces.length; i++){
+        if (i%8 !== 0 || i === 0){
+          miniArray.push(spaces[i]);
+        }else{
+          spacesCoords.push(miniArray);
+          miniArray = [];
+          miniArray.push(spaces[i]);
+        }
+      }
+
+      var coordsLength = spacesCoords.length;
+
+      for (var i = 0; i < coordsLength; i++){
+        var currentY = spacesCoords[i];
+        for (var j = 0; j < currentY.length; j++){
+          currentY[j].dataset.y = i;
+          currentY[j].dataset.x = j;
+
+          if ((j%2 === 0 && i%2 !== 0) || (j%2 !== 0 && i%2 === 0)){
+            currentY[j].className = 'space green'
           }
         }
-
-        ctx.fillRect(x, y, 50, 50);
       }
-    }
+    };
+
+    paintBoard();
   };
 
 
